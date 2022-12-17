d05dbe8e9e1cb94f6492d0c880b81bef5f820fe8
David Tolnay <dtolnay@gmail.com>
2022-12-17 10:53:39 -0800

    Release 0.4.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/db-dump/0.4.0")]
+#![doc(html_root_url = "https://docs.rs/db-dump/0.4.1")]
 #![allow(
     clippy::cast_lossless,
     clippy::cast_possible_truncation,
